6a7eb8671861e37903c0a397ee5fe882eda445a6
sea <919757890@qq.com>
2020-06-23 16:22:08 +0800

    自己开发的代码

--- demo/src/Test2.java
+++ demo/src/Test2.java
@@ -7,5 +7,8 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("test2");
+        System.out.println("test2");
+        System.out.println("test2");
+
     }
 }
