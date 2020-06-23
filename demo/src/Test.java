ea74fcd88b406f089aecbb8e04b0a2ce61a70804
sea <919757890@qq.com>
2020-06-23 12:54:40 +0800

    自己开发的代码

--- demo/src/Test.java
+++ demo/src/Test.java
@@ -1,8 +1,6 @@
 public class Test {
     public static void main(String[] args) {
     System.out.println("asdfbbbbbb");
-        System.out.println("asdfbbbbbb");
-        System.out.println("asdfbbbbbb");
 
       
 
