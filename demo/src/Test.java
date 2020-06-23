d4c2ef768fbeb6255fabaf39f1f552d353e6fa90
hxclaile <52806440+hxclaile@users.noreply.github.com>
2020-06-23 12:14:56 +0800

    Update Test.java

--- demo/src/Test.java
+++ demo/src/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("别人上传的代码");
         System.out.println("121aaaaaaaaaa111111");
-         System.out.println("别人上传的代码");
+        
 
         
     }
