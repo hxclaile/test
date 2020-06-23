44b66ef5b7c09da1055f290b8970ff80850c9e60
sea <919757890@qq.com>
2020-06-23 16:20:58 +0800

    自己开发的代码

---
+++ demo/src/Test2.java
@@ -0,0 +1,11 @@
+/**
+ * @author: Sea
+ * @create: 2020/6/23 16:20
+ * @verison: 1.0
+ * @description:
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test2");
+    }
+}
