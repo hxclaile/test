ce5c520106198f04ac4f9cb52d0e9a5c49bd61ee
hxclaile <52806440+hxclaile@users.noreply.github.com>
2020-06-23 16:12:55 +0800

    Update Test.java

--- demo/src/Test.java
+++ demo/src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-    System.out.println("aaaaaaa");
+    System.out.println("qqqq");
         System.out.println("bbbb");
         System.out.println("ccccc");
         System.out.println("dddd");
